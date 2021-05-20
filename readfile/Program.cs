2b5605857580c02ee228a52ba4d7ccbacda3905d
Raphael MANSUY <raphael.mansuy@caliatys.com>
2021-05-20 08:38:46 +0800

    first init

---
+++ readfile/Program.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.Spark.Sql;
+using static Microsoft.Spark.Sql.Functions;
+using System;
+
+namespace MySparkApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Create Spark session
+            SparkSession spark =
+                SparkSession
+                    .Builder()
+                    .AppName("word_count_sample")
+                    .GetOrCreate();
+
+            // Create initial DataFrame
+            string filePath = args[0];
+            DataFrame dataFrame = spark.Read().Text(filePath);
+
+
+            dataFrame.Show();
+
+
+            Console.Read();
+
+            //Count words
+            DataFrame words =
+                dataFrame
+                    .Select(Split(Col("value")," ").Alias("words"))
+                    .Select(Explode(Col("words")).Alias("word"))
+                    .Filter(Col("word").NotEqual(""))
+                    .GroupBy("word")
+                    .Count()
+                    .OrderBy(Col("count").Desc());
+
+            // Display results
+            words.Show();
+
+            // Stop Spark session
+            spark.Stop();
+        }
+    }
+}
---
+++ streaming/Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using Microsoft.Spark.Sql;
+using Microsoft.Spark.Sql.Streaming;
+using static Microsoft.Spark.Sql.Functions;
+
+namespace streaming
+{
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           Func<Column, Column> udfArray =
+                    Udf<string, string[]>((str) => new string[] { str, $"{str} {str.Length}" });
+
+            var hostname = "localhost";
+            var port = 65001;
+
+
+            var windowDuration = "30 seconds";
+            var slideDuration = "10 seconds";
+
+            SparkSession spark = SparkSession
+                 .Builder()
+                 .AppName("StructuredNetworkWordCountWindowed")
+                 .GetOrCreate();
+
+
+            spark.SparkContext.SetLogLevel("warn");
+
+
+            DataFrame lines = spark
+                .ReadStream()
+                .Format("socket")
+                .Option("host", hostname)
+                .Option("port", port)
+                .Load();
+
+
+            var linesWithTime = lines
+                    .WithColumn("timestamp",CurrentTimestamp())
+                    .WithColumn("DayOfTheWeek",DayOfYear(Col("timestamp")));
+
+
+
+            var words = linesWithTime
+                    .WithColumn("words",Split(Col("value")," "));
+
+           var word = words.WithColumn("word",Explode(Col("words")));
+           
+            var windowedCounts = word
+                .GroupBy(Window(Col("timestamp"), windowDuration, slideDuration),
+                   Col("word"))
+                .Count()
+                .OrderBy(Desc("window"));
+
+
+          var query = windowedCounts
+                .WriteStream()
+                .OutputMode("complete")
+                .Format("console")
+                .Option("truncate", false)
+                .OutputMode(OutputMode.Complete)
+                .Start();     
+
+            query.AwaitTermination();
+
+
+        }
+    }
+}
